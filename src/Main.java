03e4ccc407a0edea7c298dc5df16488c41c9a269
va1hrum <valeriabuhaeva13@gmail.com>
2024-11-02 15:45:16 +0300

    вторая попытка 222

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,5 @@ public static void Wprint (double[] w1) {
         String b = String.format("%.3f",num);
         System.out.print(b+"\t");
     }
+    //лллллллллллллллллллллллллллллллллллллллл
     System.out.println(' ');
